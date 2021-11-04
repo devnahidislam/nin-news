10256428be05c689b01f60669de06bdf5d29e327
mdnahidislam <mdnahidislam30@gmail.com>
2021-11-04 20:21:22 +0600

    Update

--- admin/header.php
+++ admin/header.php
@@ -2,64 +2,66 @@
 include 'config.php';
 session_start();
 
-if(!isset($_SESSION['username'])){
+if (!isset($_SESSION['username'])) {
     header("Location: {$hostName}/admin/");
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>ADMIN Panel</title>
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="../css/bootstrap.min.css" />
-        <!-- Font Awesome Icon -->
-        <link rel="stylesheet" href="../css/font-awesome.css">
-        <!-- Custom stlylesheet -->
-        <link rel="stylesheet" href="../css/style.css">
-    </head>
-    <body>
-        <!-- HEADER -->
-        <div id="header-admin">
-            <!-- container -->
-            <div class="container">
-                <!-- row -->
-                <div class="row">
-                    <!-- LOGO -->
-                    <div class="col-md-2">
-                        <a href="post.php"><img class="logo" src="images/news.jpg"></a>
-                    </div>
-                    <!-- /LOGO -->
-                    <!--User Name-->
-                    <div class="col-md-offset-3 col-md-3">
-                        <span class="admin-uname">Congrats <?php echo $_SESSION['first_name'].' '.$_SESSION['last_name']; ?> </span>
-                    </div>
-                    <!--/User Name-->
-                      <!-- LOGO-Out -->
-                    <div class="col-md-offset-3  col-md-1">
-                        <a href="logout.php" class="admin-logout">Logout</a>
-                    </div>
-                    <!-- /LOGO-Out -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>ADMIN Panel</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <!-- HEADER -->
+    <div id="header-admin">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- LOGO -->
+                <div class="col-md-2">
+                    <a href="post.php"><img class="logo" src="images/news.jpg"></a>
                 </div>
+                <!-- /LOGO -->
+                <!--User Name-->
+                <div class="col-md-offset-3 col-md-3">
+                    <span class="admin-uname">Congrats <?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?> </span>
+                </div>
+                <!--/User Name-->
+                <!-- LOGO-Out -->
+                <div class="col-md-offset-3  col-md-1">
+                    <a href="logout.php" class="admin-logout">Logout</a>
+                </div>
+                <!-- /LOGO-Out -->
             </div>
         </div>
-        <!-- /HEADER -->
-        <!-- Menu Bar -->
-        <div id="admin-menubar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                       <ul class="admin-menu">
-                            <li>
-                                <a href="post.php">Post</a>
-                            </li>
-                            <?php
-                            if($_SESSION['user_role'] == 1 ){
-                            ?>
+    </div>
+    <!-- /HEADER -->
+    <!-- Menu Bar -->
+    <div id="admin-menubar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="admin-menu">
+                        <li>
+                            <a href="post.php">Post</a>
+                        </li>
+                        <?php
+                        if ($_SESSION['user_role'] == 1) {
+                        ?>
                             <li>
                                 <a href="category.php">Category</a>
                             </li>
@@ -69,12 +71,12 @@ if(!isset($_SESSION['username'])){
                             <li>
                                 <a href="settings.php">Settings</a>
                             </li>
-                            <?php
-                            }
-                            ?>
-                        </ul>
-                    </div>
+                        <?php
+                        }
+                        ?>
+                    </ul>
                 </div>
             </div>
         </div>
-        <!-- /Menu Bar -->
+    </div>
+    <!-- /Menu Bar -->
